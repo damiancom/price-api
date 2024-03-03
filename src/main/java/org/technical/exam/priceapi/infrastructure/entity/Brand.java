87380bc6fa7cf4fd30554dd0fd6a88a1cabbd378
damiancom <damiancom@gmail.com>
2024-03-03 01:54:47 -0300

    Add repository entities

---
+++ src/main/java/org/technical/exam/priceapi/infrastructure/entity/Brand.java
@@ -0,0 +1,25 @@
+package org.technical.exam.priceapi.infrastructure.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Brand {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+
+}
---
+++ src/main/java/org/technical/exam/priceapi/infrastructure/entity/Price.java
@@ -0,0 +1,38 @@
+package org.technical.exam.priceapi.infrastructure.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "prices")
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Price {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private LocalDateTime startDate;
+  private LocalDateTime endDate;
+  private Long priceList;
+  private Long productId;
+  private Integer priority;
+  private Double price;
+  private String curr;
+
+  @ManyToOne
+  @JoinColumn(name = "brand_id")
+  private Brand brand;
+}
