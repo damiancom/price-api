a97c2aa33fef3cdc80953591313c0740fd7f7bd5
damiancom <damiancom@gmail.com>
2024-03-03 02:04:23 -0300

    Include data-source.properties in the application properties configuration.

--- src/main/java/org/technical/exam/priceapi/PriceApiApplication.java
+++ src/main/java/org/technical/exam/priceapi/PriceApiApplication.java
@@ -2,8 +2,10 @@ package org.technical.exam.priceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:data-source.properties")
 public class PriceApiApplication {
 
   public static void main(String[] args) {
