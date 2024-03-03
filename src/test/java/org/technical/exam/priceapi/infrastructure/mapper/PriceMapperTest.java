3ce81079ebf2664552693e2abd3d1c024a9dec28
damiancom <damiancom@gmail.com>
2024-03-03 17:53:53 -0300

    Added test for PriceMapper in infrastructure

---
+++ src/test/java/org/technical/exam/priceapi/infrastructure/mapper/PriceMapperTest.java
@@ -0,0 +1,59 @@
+package org.technical.exam.priceapi.infrastructure.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mapstruct.factory.Mappers;
+import org.technical.exam.priceapi.domain.model.PriceModel;
+import org.technical.exam.priceapi.infrastructure.entity.Brand;
+import org.technical.exam.priceapi.infrastructure.entity.Price;
+
+class PriceMapperTest {
+
+  private final PriceMapper priceMapper = Mappers.getMapper(PriceMapper.class);
+
+  @Test
+  @DisplayName("Should return null when the Price list is null.")
+  void shouldReturnNullWhenPriceListIsNull() {
+    List<PriceModel> priceModelList = priceMapper.toListModel(null);
+
+    assertNull(priceModelList);
+  }
+
+  @Test
+  @DisplayName("Should return an empty list when the Price list is empty.")
+  void shouldReturnEmptyListWhenPriceListIsEmpty() {
+    List<Price> priceList = List.of();
+
+    List<PriceModel> priceModelList = priceMapper.toListModel(priceList);
+
+    assertEquals(0, priceModelList.size());
+  }
+
+  @Test
+  @DisplayName("Should return a list of PriceModel when the Price list has elements.")
+  void shouldReturnListOfPriceModelWhenPriceListHasElements() {
+    Brand brand = mock(Brand.class);
+    Price price = mock(Price.class);
+    price.setBrand(brand);
+
+    when(price.getBrand()).thenReturn(brand);
+
+    List<Price> priceList = List.of(
+        mock(Price.class),
+        price,
+        mock(Price.class)
+    );
+
+    List<PriceModel> priceModelList = priceMapper.toListModel(priceList);
+
+    assertEquals(priceList.size(), priceModelList.size());
+    assertEquals(brand.getId(), priceList.get(1).getBrand().getId());
+  }
+
+}
