acb7972af18137e7cc17727455d8257ce7289575
damiancom <damiancom@gmail.com>
2024-03-03 01:04:30 -0300

    add scaffolding

---
+++ src/test/java/org/technical/exam/priceapi/PriceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.technical.exam.priceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
