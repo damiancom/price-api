20559f72aa1f1e28dc54d4b11efa38ea8d870d23
damiancom <damiancom@gmail.com>
2024-03-03 05:34:14 -0300

    Remove data-source.properties from PriceApiApplication

--- src/main/java/org/technical/exam/priceapi/PriceApiApplication.java
+++ src/main/java/org/technical/exam/priceapi/PriceApiApplication.java
@@ -2,10 +2,8 @@ package org.technical.exam.priceapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:data-source.properties")
 public class PriceApiApplication {
 
   public static void main(String[] args) {
