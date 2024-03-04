2bee951de8976f5d82b518220b7cec00720e2f42
damiancom <damiancom@gmail.com>
2024-03-03 22:00:25 -0300

    Added exception handler to manage service responses in case of errors

---
+++ src/main/java/org/technical/exam/priceapi/application/rest/exception/ControllerErrorHandler.java
@@ -0,0 +1,62 @@
+package org.technical.exam.priceapi.application.rest.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.technical.exam.priceapi.application.rest.response.ErrorResponse;
+
+@Slf4j
+@ControllerAdvice
+@RestControllerAdvice
+public class ControllerErrorHandler {
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorResponse> handleException(Exception e) {
+    log.error("[APPLICATION] An error occurred: {}", e.getMessage(), e);
+
+    ErrorResponse apiResponse = ErrorResponse.builder()
+        .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+        .detail("An error occurred")
+        .build();
+
+    return new ResponseEntity<>(apiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(DataAccessException.class)
+  public ResponseEntity<ErrorResponse> handleDataAccessException(DataAccessException e) {
+    log.error("[DATABASE] {}", e.getMessage(), e);
+
+    ErrorResponse apiResponse = ErrorResponse.builder()
+        .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
+        .detail("An unexpected error occurred")
+        .build();
+
+    return new ResponseEntity<>(apiResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler({
+      MissingServletRequestParameterException.class,
+      MethodArgumentTypeMismatchException.class,
+      IllegalArgumentException.class
+  })
+  public ResponseEntity<ErrorResponse> handleRequestParameterException(Exception e) {
+    log.error("[BAD_REQUEST] {}", e.getMessage(), e);
+
+    ErrorResponse apiResponse = ErrorResponse.builder()
+        .status(HttpStatus.BAD_REQUEST.value())
+        .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+        .detail(e.getMessage())
+        .build();
+
+    return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
+  }
+
+}
