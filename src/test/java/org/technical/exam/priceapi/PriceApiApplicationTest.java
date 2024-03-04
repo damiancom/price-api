517bde72e36c28d3e1552d17436ca059a9d2f522
damiancom <damiancom@gmail.com>
2024-03-03 21:03:44 -0300

    Renamed PriceApiApplicationTest class to singular

--- src/test/java/org/technical/exam/priceapi/PriceApiApplicationTest.java
+++ src/test/java/org/technical/exam/priceapi/PriceApiApplicationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PriceApiApplicationTests {
+class PriceApiApplicationTest {
 
   @Test
   void contextLoads() {
